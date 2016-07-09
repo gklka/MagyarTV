9f9cfe91b16aa25af8212b0dd1cb22854287df69
Gruber Kristóf <gk@lka.hu>
2016-07-09 20:28:37 +0200

    Initial Commit

---
+++ MagyarTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagyarTV
+//
+//  Created by GK on 2016.07.09..
+//  Copyright © 2016. GKSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagyarTV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagyarTV
+//
+//  Created by GK on 2016.07.09..
+//  Copyright © 2016. GKSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
