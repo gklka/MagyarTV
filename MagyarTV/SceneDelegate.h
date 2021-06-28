44e809c43121a3e61e95bf09eabf45e0a3794638
Gruber Kristóf <gk@lka.hu>
2021-06-28 11:48:03 +0200

    Added support for multiple windows on iOS and macOS

---
+++ MagyarTV/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MagyarTV
+//
+//  Created by GK on 2021.06.28..
+//  Copyright © 2021. GKSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
